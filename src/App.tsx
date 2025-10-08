ed5e7bad0f04cffb2c82df5c637bfccee71e9f2d
kartiksathwara <kartiksathwara64@gmail.com>
2025-10-08 16:36:00 +0530

    changes1

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+   
+      <h1 className="text-3xl font-bold underline bg-black text-white">
+      Hello world!
+    </h1>
+  
+  )
+}
+
+export default App
